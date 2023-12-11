d92b0cd6df81934165de15a40b62d9e76e94092d
Peter Gallwas <peter@husky.nz>
2023-12-11 15:40:42 +1300

    add system power function code

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,18 +1,13 @@
 ﻿#include <iostream>
 #include "caulc.h"
 #include "game.h"
-#include "web.h"
+#include "system.h"
 using namespace std;
 
 int main() {
-    int action;
-    
-    
-    cout << "What Action Would You Like To Peform (1: Calculator 2: Game 3: Reboot your system 4: Exit):  ";
+    int action;cout << "What Action Would You Like To Peform (1: Calculator 2: Game 3: Perform power oprations on your system 4: Exit):  ";
     cin >> action;
-    
-    
-    
+
     if (action == 1) {
         cal();
     }
@@ -21,7 +16,7 @@ int main() {
         game();
     }
     else if (action == 3) {
-        web();
+        systemaction();
         
     }
     else if (action == 4) {
---
+++ Project1/system.h
@@ -0,0 +1,36 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include <iostream>
+using namespace std;
+
+inline void systemaction() { // Assuming void return type
+    int x, y;
+    int sum;
+    int operation;
+
+    cout << R"(
+HuskyNZ's Simple C++ Calculator                                                                                                                                                                                                                                                                                                                                                                                                                                     
+)" << '\n';
+
+    cout << "What operation would you like to perform? (1: Reboot, 2: Shutdown, 3: Sleep): ";
+    cin >> operation;
+
+    if (operation == 1) {
+
+        system("shutdown /r /t 0");
+    }
+    else if (operation == 2) {
+        system("shutdown -s");
+    }
+    else if (operation == 3) {
+        system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0");
+    }
+    else {
+        cout << "Invalid operation. Please try again.";
+    }
+}
+
+#endif SYSTEM_H
+
+
